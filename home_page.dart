369f18bedf1238cd28a8b1fc6077b3ca0d7e2f14
ikahsin <144208357+ikahsin@users.noreply.github.com>
2023-11-14 12:45:58 +0300

    Add files via upload

---
+++ home_page.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      body:  SafeArea(
+        bottom: false,
+        child: Center(
+          child: Column(
+
+            textDirection: TextDirection.ltr,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            verticalDirection: VerticalDirection.down,
+
+            children: <Widget>[
+              Expanded(
+                child:
+                Row(
+                  children: <Widget>[
+                    Expanded(child:
+                    Container(
+                      color: Colors.black,
+                      margin: EdgeInsets.only(left: 8, top: 8, right: 4, bottom: 4),
+                      child: Center(
+                        child: Text('1',style: TextStyle(color: Colors.green),),
+                      ),
+
+                    ),
+                    ),
+                    Expanded(child: Container(color: Colors.yellow, margin: EdgeInsets.only(left: 4, top: 8, right: 8, bottom: 4),
+                      child: Center(
+                        child: Text('1',style: TextStyle(color: Colors.green),),
+                      ),),
+
+                    )
+                    ,
+                  ],
+                ),
+                flex: 3,
+              ),
+              Expanded(
+                  child: Row(
+                    children: [
+                      Expanded(child: Container(color: Colors.red, margin: EdgeInsets.only(left: 8, top: 4, right: 4, bottom: 4),
+                        child: Center(
+                          child: Text('1',style: TextStyle(color: Colors.green),),
+                        ),)
+                      ),
+                      Expanded(child: Container(color: Colors.orange, margin: EdgeInsets.only(left: 4, top: 4, right: 8, bottom: 4),
+                        child: Center(
+                          child: Text('1',style: TextStyle(color: Colors.green),),
+                        ),)
+                      ),
+                    ],
+                  ),
+                  flex:1
+              ),
+              Expanded(
+                  child: Row(
+                    children: [
+                      Expanded(child: Container(color: Colors.indigoAccent, margin: EdgeInsets.only(left: 8, top: 4, right: 4, bottom: 4),
+                        child: Center(
+                          child: Text('1',style: TextStyle(color: Colors.green),),
+                        ),)
+                      ),
+                      Expanded(child: Container(color: Colors.cyan, margin: EdgeInsets.only(left: 4, top: 4, right: 8, bottom: 4),
+                        child: Center(
+                          child: Text('1',style: TextStyle(color: Colors.green),),
+                        ),)
+                      ),
+                    ],
+                  ),
+                  flex:1
+              ),
+              Expanded(
+                child:
+                Row(
+                  children: <Widget>[
+                    Expanded(child: Container(color: Colors.teal, margin: EdgeInsets.only(left: 8, top: 4, right: 4, bottom: 8),
+                      child: Center(
+                        child: Text('1',style: TextStyle(color: Colors.green),),
+                      ),)
+                    ),
+                    Expanded(child: Container(color: Colors.grey, margin: EdgeInsets.only(left: 4, top: 4, right: 8, bottom: 8),
+                      child: Center(
+                        child: Text('1',style: TextStyle(color: Colors.green),),
+                      ),)
+                    ),
+                  ],
+                ),
+                flex: 3,
+              ),
+            ],
+          ),
+        ),
+      ),
+
+    );
+  }
+}
